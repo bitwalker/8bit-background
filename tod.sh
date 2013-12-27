76d3ad2913fb929187c98d0770733e76d1286576
Paul Schoenfelder <paulschoenfelder@gmail.com>
2013-12-27 00:01:37 -0600

    Add desktop resolution detection. Automatically sets the appropriate image for your monitor's res

--- tod.sh
+++ tod.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 MONITOR=
+# Parse args
 while getopts "m:a" OPTION
 do
   case $OPTION in
@@ -31,42 +32,55 @@ fi
 
 # Get the script directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Get the number of monitors
+NUM_MONITORS=$(osascript -e "tell application \"System Events\" to get count of (a reference to every desktop)")
+# Get the list of desktop resolutions
+resolutions=(
+  $(/usr/sbin/system_profiler SPDisplaysDataType | grep Resolution | awk '{print $2x$3$4}')
+)
 
 # This function will execute some AppleScript to set the background
 function setbg {
-  osascript -e "
-    tell application \"System Events\"
-      set allDesktops to a reference to every desktop
-      set numDesktops to count of allDesktops
-      if $MONITOR equals -1 then
-        repeat with x from 1 to numDesktops
-          set picture of (item x of allDesktops) to file (POSIX file \"$DIR/$1\") as alias
-        end repeat
-      else
-        if $MONITOR <= numDesktops then
-          set picture of (item $MONITOR of allDesktops) to file (POSIX file \"$DIR/$1\") as alias
-        end if
-      end if
-    end tell
-  "
+  if [ $MONITOR -eq -1 ]; then
+    for i in {0..$NUM_MONITORS}
+    do
+      RES=${resolutions[i]}
+      CURRENT_DESKTOP=$((i+1))
+      osascript -e "
+        tell application \"System Events\"
+          set allDesktops to a reference to every desktop
+          set picture of (item $CURRENT_DESKTOP of allDesktops) to file (POSIX file \"$DIR/images/$RES/$1\") as alias
+        end tell
+      "
+    done
+  else
+    if [ $MONITOR -le $NUM_MONITORS ]; then
+      RES_INDEX=$(($MONITOR-1))
+      RES=${resolutions[$RES_INDEX]}
+      osascript -e "
+        tell application \"System Events\"
+          set allDesktops to a reference to every desktop
+          set picture of (item $MONITOR of allDesktops) to file (POSIX file \"$DIR/images/$RES/$1\") as alias
+        end tell
+      "
+    fi
+  fi
 }
 
 # Paths to all of the background images
 files=(
-  images/1-early-morning.jpg
-  images/2-morning.jpg
-  images/3-late-morning.jpg
-  images/4-afternoon.jpg
-  images/5-late-afternoon.jpg
-  images/6-evening.jpg
-  images/7-late-evening.jpg
-  images/8-night.jpg
-  images/9-late-night.jpg
+  01-Morning.png
+  02-Late-Morning.png
+  03-Afternoon.png
+  04-Late-Afternoon.png
+  05-Evening.png
+  06-Late-Evening.png
+  07-Night.png
+  08-Late-Night.png
 )
 
 # Timings for the backgrounds, in order
 timing=(
-  4
   7
   10
   12
@@ -82,7 +96,7 @@ hour=`date +%H`
 
 # Loop over every timing, if the current hour is greater than or equal
 # to the timing value, set the background to the corresponding image
-for i in {8..0..-1}
+for i in {7..0..-1}
 do
   if [ $hour -ge ${timing[i]} ]; then
     # Set the background to the appropriate image
@@ -92,4 +106,4 @@ do
 done
 
 # If no match is found for the hour, set the background to late-night.
-setbg ${files[8]}
+setbg ${files[7]}
